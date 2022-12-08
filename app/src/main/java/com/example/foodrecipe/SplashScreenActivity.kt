e1c1f5ed366b35f064eea13b0ddb15e2844e1781
Abdelrahman Essawy <boda.m7amd@gmail.com>
2022-12-08 11:40:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/foodrecipe/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.example.foodrecipe
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.ImageView
+
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var image: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        image = findViewById(R.id.iv_note)
+        image.alpha = 0f
+        image.animate().setDuration(4000) .alpha(1f) .withEndAction(){
+
+            val intent =Intent(this,Login::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+
+            finish()
+        }
+    }
+}
